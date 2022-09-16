c4c98c57a2d8b59a0357d40ad58f604cf06e7b38
AnastasiyaShiro <erza.ychiha2015@yandex.ru>
2022-09-16 12:39:59 +0300

    Рабочая версия

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -2,25 +2,25 @@ package com.example.demo;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
-    @FXML
-    private Label Calculation;
-    private Label Percent15;
-    private Label Percent10;
-    private Label Percent5;
+    public TextField Calculate;
+    public Label Percent15;
+    public Label Percent10;
+    public Label Percent5;
     private Percent perc = new Percent();
 
     @FXML
     protected void onHelloButtonClick()
     {
 
-        double text=Double.parseDouble(Calculation.getText());
+        double text=Double.parseDouble(Calculate.getText());
         perc.countSumTrunc(text,15);
-        Percent15.setText(perc+" рублей.");
+        Percent15.setText(perc.toString());
         perc.countSumTrunc(text,10);
-        Percent10.setText(perc+" рублей.");
+        Percent10.setText(perc.toString());
         perc.countSumTrunc(text,5);
-        Percent5.setText(perc+" рублей.");
+        Percent5.setText(perc.toString());
     }
 }
--- src/main/java/com/example/demo/Percent.java
+++ src/main/java/com/example/demo/Percent.java
@@ -3,6 +3,15 @@ package com.example.demo;
 public class Percent
 {
     private double Calc;
+
+    public double getCalc() {
+        return Calc;
+    }
+
+    public void setCalc(double calc) {
+        Calc = calc;
+    }
+
     public Percent()
     {
         Calc=0.0;
@@ -16,12 +25,18 @@ public class Percent
     public double countSum(double sum, int pr)  //возвращает значение итоговой суммы с учетом заданного % pr
     // от суммы sum
     {
-        return sum+countPr(sum,pr)/100;
+        return sum+countPr(sum,pr);
     }
 
     public int countSumTrunc(double sum, int pr) //округленное до ближайшего целого значение итоговой суммы
     // с учетом заданного % pr от суммы sum
     {
+        setCalc((int)Math.round(countSum(sum,pr)));
         return (int)Math.round(countSum(sum,pr));
+    }
+
+    @Override
+    public String toString() {
+        return Calc + " рублей." ;
     }
 }
